45e27e64531f9b76375f77c35fd7ddb41a63c28b
Ye Gao <gaoyescm@meta.com>
2025-04-24 13:03:36 -0400

    Add trick 3 for shell initialization

--- tricks.sh
+++ tricks.sh
@@ -3,3 +3,13 @@
 echo $PATH | tr : \\n
 
 # trick 2: use absolute path for symlink 
+
+# trick 3: bash initialization (ref: man bash) 
+# --------------------------------------------------
+# |                 |     login        | non-login |
+# |------------------------------------------------|
+# |     interactive | /etc/profile     | ~/.bashrc |
+# |-----------------| ~/.bash_profile* |-----------|
+# | non-interactive | ~/.bash_login    | BASH_ENV  |
+# |                 | ~/.profile       |           |
+# --------------------------------------------------
